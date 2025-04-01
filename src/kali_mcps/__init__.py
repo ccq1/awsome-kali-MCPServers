4652afd4ef6d2a4383ad3a7f9a6adec8d1e4a0fc
ccq <chaoqun451@gamil.com>
2025-04-01 18:07:05 +0000

    same name

--- src/kali_mcps/__init__.py
+++ src/kali_mcps/__init__.py
@@ -3,16 +3,16 @@ Kali MCP Servers package
 """
 from src.kali_mcps.base.kali_command import CommandRunner
 from src.kali_mcps.objdump.actions import (
-    file_headers,
-    disassemble,
-    symbol_table,
-    section_headers,
+    file_headers_action,
+    disassemble_action,
+    symbol_table_action,
+    section_headers_action,
 )
 
 __all__ = [
     "CommandRunner",
-    "file_headers",
-    "disassemble",
-    "symbol_table",
-    "section_headers",
+    "file_headers_action",
+    "disassemble_action",
+    "symbol_table_action",
+    "section_headers_action",
 ]
